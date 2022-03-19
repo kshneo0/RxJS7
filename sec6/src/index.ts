7d93d03bd88666e3facbe7c2ebe767906ef1c1a1
kshneo0 <kshneo@gmail.com>
2022-03-19 10:10:32 +0900

    52. debounceTime

--- sec6/src/index.ts
+++ sec6/src/index.ts
@@ -1,36 +1,13 @@
-import { of } from "rxjs";
-import { filter, map, tap } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { debounceTime, map } from "rxjs/operators";
 
-// 50. tap
-// 5보다 큰 값을 2배해서 출력
-/*
-of(1, 7, 3, 6, 2)
+// 52. debounceTime
+
+const sliderInput = document.querySelector("input#slider");
+
+fromEvent<any>(sliderInput, "input")
   .pipe(
-    map((value) => value * 2),
-    tap((value) => console.log("Spy:", value)),
-    filter((value) => value > 5),
+    debounceTime(2000),
+    map((event) => event.target["value"])
   )
-  .subscribe((value) => console.log("Output:", value));
-*/
-/*
-  of(1, 7, 3, 6, 2)
-    .pipe(
-      filter((value) => value > 5),
-      tap((value) => console.log("Spy:", value)),
-      map((value) => value * 2),
-    )
-    .subscribe((value) => console.log("Output:", value));
-*/
-
-of(1, 7, 3, 6, 2)
-  .pipe(
-    filter((value) => value > 5),
-    map((value) => value * 2),
-    tap({
-      next: (value) => console.log("Spy:", value),
-    })
-  )
-  .subscribe((value) => console.log("Output:", value));
-
-// tap을 사용하면 Observable이 실행되지 않는다
-// https://jaywoz.medium.com/information-is-king-tap-how-to-console-log-in-rxjs-7fc09db0ad5a
+  .subscribe((value) => console.log(value));
